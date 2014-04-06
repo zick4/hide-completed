dcf027c6ff668b3aaff72a839de7946748bb511f
Paweł Grzeszczak <pawel.grzeszczak@nextwebventures.com>
2014-04-06 02:46:40 +0200

    dodano filtr okręślający czy chować zablokowane zadania

--- hidecompleted.js
+++ hidecompleted.js
@@ -50,7 +50,6 @@ BBLog.handle("add.plugin", {
             var isBf3 = Boolean(document.URL.match(/bf3\/.*assignments.*$/)),
                 isBf4 = Boolean(document.URL.match(/bf4\/.*assignments.*/));
 
-            delete(plugin.adapterH);
             // wczytanie odpowiedniego adaptera
             if (isBf3) {
                 plugin.adapterH = new Bf3AdapterH(plugin);
